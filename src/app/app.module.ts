7b1ae51e1c5ae6740bb4c3bb77ee1e894ba500e6
LloydNicholson <nichlloyd@gmail.com>
2019-02-12 12:58:14 +0200

    changes made to typescript to refactor code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { GamesComponent } from './projects/games/games.component';
+import { ApplicationsComponent } from './projects/applications/applications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    GamesComponent,
+    ApplicationsComponent
   ],
   imports: [
     BrowserModule
