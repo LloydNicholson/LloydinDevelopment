c1ef31fdb59c4b14fcdb9ed55aa49e897d9dc7fc
LloydNicholson <nichlloyd@gmail.com>
2019-02-15 16:41:08 +0200

    made changes to game and application links

--- src/app/projects/games/games.component.ts
+++ src/app/projects/games/games.component.ts
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
   games = [
-    {name: 'Noob Trainer', url: 'https://rustylloyd.itch.io/noobtrainer2018',
-      imageUrl: 'https://lloydindevelopment.com/wp-content/uploads/2018/09/Noob-Trainer-Icon.png'},
-    {name: 'Sluggers', url: 'https://rustylloyd.itch.io/sluggers',
-      imageUrl: 'https://lloydindevelopment.com/wp-content/uploads/2018/09/Screen-Shot-2018-07-17-at-18.36.52.png'},
+    {name: 'Noob Trainer',
+      url: 'https://rustylloyd.itch.io/noobtrainer2018',
+      imageUrl: 'https://lloydindevelopment.com/wp-content/uploads/2018/09/Noob-Trainer-Icon.png',
+    details: 'A fun, colourful platformer'},
+    {name: 'Sluggers',
+      url: 'https://rustylloyd.itch.io/sluggers',
+      imageUrl: 'https://lloydindevelopment.com/wp-content/uploads/2018/09/Screen-Shot-2018-07-17-at-18.36.52.png',
+      details: 'A semi-finished platformer'},
   ];
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
