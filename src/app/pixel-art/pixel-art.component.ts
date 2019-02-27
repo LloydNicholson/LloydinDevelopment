63da6f1d2cb8c7f3d6d8342f479ee9d235494ab5
LloydNicholson <nichlloyd@gmail.com>
2019-02-27 11:27:32 +0200

    deployed with new look and design of pixel-art page and new look of projects page

--- src/app/pixel-art/pixel-art.component.ts
+++ src/app/pixel-art/pixel-art.component.ts
@@ -14,6 +14,10 @@ export class PixelArtComponent implements OnInit {
     {name: 'Iron Man', date: new Date(2018, 7 - 1, 18), imgUrl: 'assets/img/Iron Man no_bg.png'},
     {name: 'Hulk', date: new Date(2018, 7 - 1, 23), imgUrl: 'assets/img/Hulk_enlarged no_bg.png'},
     {name: 'Black Widow', date: new Date(2018, 7 - 1, 25), imgUrl: 'assets/img/Black Widow no_bg.png'},
+    {name: 'Blue House Games', date: new Date(2018, 10 - 1, 20), imgUrl: 'assets/img/Blue house.png'},
+    {name: 'Orange', date: new Date(2018, 7 - 1, 25), imgUrl: 'assets/img/Orange large.png'},
+    {name: 'Potion', date: new Date(2018, 7 - 1, 25), imgUrl: 'assets/img/Potion large.png'},
+    {name: 'Jo\'burg City Skyline', date: new Date(2018, 7 - 1, 25), imgUrl: 'assets/img/Johannesburg City Skyline.png'},
 
   ];
   constructor() { }
